656aeaaa00ab47a1339fe792f81ef5b80e7b48a6
pyksoft <pyksoft@gmail.com>
2017-10-14 22:21:11 +1100

    Add delete method

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,24 +1,29 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
 
+  # Shows a list of Conversations
   def index
-
     @conversations = Conversation.all
-
   end
 
+  # Creates a new Conversation or Joins an existing Conversation
   def create
-    
+    # Get the topic
     topic = params[:topic]
 
+    # Search for the topic in Conversation table
     conversation = Conversation.where(topic: topic).first
     
+    # If topic does not exists, create the topic
     if !conversation
       @conversation = Conversation.new(topic: topic)
       @conversation.save
-    else
 
+    # If topic exists, join the conversation
+    else
       @conversation = Conversation.find(conversation.id)
+
+      # If user is not yet in conversation, add user to conversation
       unless current_user.has_joined?(conversation)
         @conversation.users << current_user
         @conversation.save
@@ -26,12 +31,22 @@ class ConversationsController < ApplicationController
       
     end
 
+    # Redirect to conversation show page
     redirect_to conversation_path(@conversation)
   end
 
-  def show    
+  # Shows the conversation and its messages
+  def show
     @conversation = Conversation.find(params[:id])
-    # @message = Message.new
+  end
+
+  # Deletes a conversation and its messages
+  def destroy
+    @conversation = Conversation.find(params[:id])
+    @conversation.destroy
+
+    # Redirect to index
+    redirect_to conversations_path
   end
 
 end
