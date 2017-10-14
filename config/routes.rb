88327b2aa875a52f8d38c0911fe7f7b49c64142f
pyksoft <pyksoft@gmail.com>
2017-10-14 22:25:06 +1100

    Update message update path and route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  patch 'messages/update'
-
   root 'conversations#index'
-  resources 'conversations', only: [:index, :create, :show, :update]
+  resources 'conversations', only: [:index, :create, :show, :destroy]
+  resources 'messages', only: [:update]
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
